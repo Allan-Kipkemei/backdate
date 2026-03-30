4a5a03781f2079d89a83a2805063b0e5176658d9
Allan Kiplagat <allankiplgat@gmail.com>
2026-03-30 15:43:08 +0300

    I changes OS

--- ponzi/myapp.jsx
+++ ponzi/myapp.jsx
@@ -1,3 +1,31 @@
-import "<React>
+import os
+import datetime
+import time
 
-/React>
+# File to modify (so commits have changes)
+FILE_NAME = "app.py"
+
+# Number of commits you want
+NUM_COMMITS = 5
+
+# Start date (past)
+start_date = datetime.datetime(2024, 1, 1, 9, 0, 0)
+#iterate num commit 
+for i in range(NUM_COMMITS):
+
+    commit_date = start_date + datetime.timedelta(days=i*2)
+
+    # Make a small change to file
+    with open(FILE_NAME, "a") as f:
+        f.write(f"# Update {i} at {formatted_date}\n")
+
+
+    os.system("git add .")
+
+    command = f'''
+    GIT_AUTHOR_DATE="{formatted_date}" GIT_COMMITTER_DATE="{formatted_date}" git commit -m "Update {i}"
+    '''
+
+
+
+
